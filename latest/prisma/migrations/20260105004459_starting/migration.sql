46b5645083d110f13dbbfcede156a3927b113062
saikuch31 <saikuchulakanti@gmail.com>
2026-01-04 19:45:29 -0500

    slightly updated db

---
+++ latest/prisma/migrations/20260105004459_starting/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "WorkoutEntry" ADD COLUMN "reps" INTEGER;
+ALTER TABLE "WorkoutEntry" ADD COLUMN "sets" INTEGER;
+ALTER TABLE "WorkoutEntry" ADD COLUMN "totalWeight" INTEGER;
+ALTER TABLE "WorkoutEntry" ADD COLUMN "weight" INTEGER;
